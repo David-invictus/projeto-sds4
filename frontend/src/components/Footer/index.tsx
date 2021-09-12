28f02ac6419671ee6c91a5833c25ae19836afe07
david-invictus <davidfpessoa02@gmail.com>
2021-09-12 17:28:44 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Aplicativo desenvolvido por <a href="https://github.com/David-invictus" target="_blank" rel="noreferrer">Invictus</a></p>
+                <p className="text-light"><small><strong>Semana Spring React (SDS 4.0)</strong><br/>
+    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
